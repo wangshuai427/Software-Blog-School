08bf2f909f23f56a7153e34e5c73ea6fbcd4d8bb
wangshuai427 <244580440@qq.com>
2011-07-19 10:41:17 +0800

    initialize SSB project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoftwareSchoolBlog::Application.config.secret_token = '15e428d19733556571dfdf1b7ed16dcc5eb426873a7d5649ee7e1a9b2fa82ad678a79611ac08d5ba3fce5c535cb70d4388028aad02bb84bebcdb67aefebec489'
